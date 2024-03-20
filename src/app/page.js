164f3164b6b8f13520343ccdc0e36bace74a2fd3
giancarlobrusca <giancarlobrusca@gmail.com>
2024-03-20 10:37:43 -0300

    base

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,128 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+import styled from "styled-components";
+import { AnimatePresence, motion } from "framer-motion";
+import { FaEnvelope, FaXTwitter } from "react-icons/fa6";
+
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+});
 
 export default function Home() {
+  const [clicked, setClicked] = useState(false);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <AnimatedCursor
+        color="0,0,0"
+        innerSize={8}
+        outerSize={35}
+        innerScale={2}
+        outerScale={3}
+        outerAlpha={1}
+        outerStyle={{
+          backgroundColor: "#fff",
+          mixBlendMode: "exclusion",
+        }}
+        clickables={["h1", "a"]}
+      />
+      <Main>
+        <div />
+        <div style={{ textAlign: "center" }}>
+          <h1
+            style={{
+              color: clicked ? "black" : "white",
+              transition: "all 0.3s ease-in-out",
+            }}
+            onClick={() => setClicked((current) => !current)}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            giancarlo brusca.
+          </h1>
+          <h2
+            style={{
+              color: clicked ? "darkgray" : "#131313",
+              transition: "all 0.3s ease-in-out",
+            }}
+          >
+            frontend developer
+          </h2>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <Socials
+          style={{
+            color: clicked ? "black" : "white",
+            transition: "all 0.3s ease-in-out",
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <li>
+            <a href="https://twitter.com/giancarlobrusca">
+              <FaXTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:giancarlobrusca@gmail.com">
+              <FaEnvelope />
+            </a>
+          </li>
+        </Socials>
+      </Main>
+      <AnimatePresence>
+        {clicked && (
+          <motion.div
+            initial={{
+              backgroundColor: "white",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              borderRadius: 60,
+              opacity: 0,
+              zIndex: 0,
+            }}
+            animate={{ opacity: 1, width: "100vw", height: "100vh" }}
+            exit={{ opacity: 0 }}
+          />
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {!clicked && (
+          <motion.div
+            initial={{
+              backgroundColor: "black",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              borderRadius: 60,
+              opacity: 0,
+              zIndex: 0,
+            }}
+            animate={{ opacity: 1, width: "100vw", height: "100vh" }}
+            exit={{ opacity: 0 }}
+          />
+        )}
+      </AnimatePresence>
+    </>
   );
 }
+
+const Main = styled.main`
+  position: relative;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 50px;
+  z-index: 1;
+`;
+
+const Socials = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 50px;
+  font-size: 1em;
+  margin-bottom: 50px;
+`;
